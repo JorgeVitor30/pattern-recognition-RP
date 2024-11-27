27bb1e4a8de40010e01da1e506ed42261ba8cf0c
JorgeVitor30 <jorgevitorlima8@gmail.com>
2024-11-27 13:44:54 -0300

    add logistic regression, start

---
+++ models/RegressãoLogistica/model.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import io
+from sklearn.utils import shuffle
+
+
+class LogisticRegression:
+    def __init__(self, n_epochs: int = 1000, learning_rate: float = 0.001):
+        self.n_epochs = n_epochs
+        self.learning_rate = learning_rate
+        self.w = None
+
+    def _sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def fit(self, X: pd.DataFrame, y: pd.DataFrame):
+        if (len(X) != len(y)): return False
+        self.w = np.zeros(X.shape[1])
+
+        for _ in range(0, self.n_epochs):
+            X, y = shuffle(X, y)
+            for i in range(len(X)):
+                xi = X.iloc[i]
+                yi = y.iloc[i]
+
+                error = yi - self._sigmoid(np.dot(xi, self.w))
+                self.w += self.learning_rate * error * xi
+            
+    def predict(self, X: pd.DataFrame):
+        y_pred = self._sigmoid(np.dot(X, self.w) )
+        return np.round(y_pred)
+    
+    def calcule_metrics(self, y_real: np.ndarray, y_pred: np.ndarray):
+        if (len(y_real) != len(y_pred)): return False
+
+        if isinstance(y_real, pd.Series): y_real = y_real.to_numpy()
+        if isinstance(y_pred, pd.Series): y_pred = y_pred.to_numpy()
+
+        true_positive = 0
+        true_negative = 0
+        false_positive = 0
+        false_negative = 0
+
+        acurracy = 0
+
+        for i in range(len(y_real)):
+            if y_real[i] == 1 and y_pred[i] == 1:
+                true_positive += 1
+            elif y_real[i] == 0 and y_pred[i] == 1:
+                false_positive += 1
+            elif y_real[i] == 0 and y_pred[i] == 0:
+                true_negative += 1
+            elif y_real[i] == 1 and y_pred[i] == 0:
+                false_negative += 1
+            
+            if (y_pred[i] == y_real[i]):
+                acurracy +=1
+
+        
+        return {"TP": true_positive, "TN": true_negative, "FP": false_positive, "FN": false_negative, "AC": acurracy / len(y_pred)}
